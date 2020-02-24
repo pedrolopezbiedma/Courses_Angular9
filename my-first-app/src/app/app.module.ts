a9ad7a581e43a90b59a7815a162408dc42d4dff0
Pedro <pedrolopezbiedma@gmail.com>
2020-02-24 10:20:35 +0100

    Creating some other components and including the first assignment

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { ErrorAlertComponent } from './erroralert/erroralert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.compoment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    ErrorAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ my-first-app/src/app/erroralert/erroralert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-erroralert',
+    templateUrl: './erroralert.component.html',
+    styleUrls: ['./erroralert.component.css']
+})
+export class ErrorAlertComponent {
+
+}
