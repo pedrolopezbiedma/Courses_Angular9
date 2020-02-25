7d8ca4097b3b6673091253a7a79a42ab4a763d71
Pedro <pedrolopezbiedma@gmail.com>
2020-02-25 13:07:38 +0100

    Including changes from lessons about data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -5,5 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html'
 })
 export class ServersComponent {
+  addNewServerStatus = false;
+  creationStatus = '';
+  serverName = '';
+
+  getAddNewServerStatus() {
+    return this.addNewServerStatus;
+  }
+
+  onServerCreation() {
+    this.creationStatus = 'The server ' + this.serverName + ' was created';
+  }
 
 }
