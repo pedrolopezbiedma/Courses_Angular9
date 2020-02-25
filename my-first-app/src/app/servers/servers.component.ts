e47d2e5e521d8351fd644fc5ad7059db179d19ce
Pedro <pedrolopezbiedma@gmail.com>
2020-02-25 13:29:47 +0100

    Including assignment from Databinding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,13 +8,25 @@ export class ServersComponent {
   addNewServerStatus = false;
   creationStatus = '';
   serverName = '';
+  serverOwner = '';
 
   getAddNewServerStatus() {
     return this.addNewServerStatus;
   }
-
   onServerCreation() {
     this.creationStatus = 'The server ' + this.serverName + ' was created';
   }
+
+  getClearButtonStatus() {
+    if (this.serverOwner === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  onResetServerOwner() {
+    this.serverOwner = '';
+  }
+
 
 }
