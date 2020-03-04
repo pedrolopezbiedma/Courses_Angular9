116f2a4ec991218ab6898dab983c198644172ef8
Pedro <pedrolopezbiedma@gmail.com>
2020-03-04 16:19:13 +0100

    Finishing last assignment on Section 2, let's go Section 3!!

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html'
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
   servers = ['Test server 1', 'Test server 2'];
   serverName = '';
+  secretShown = false;
+  eventsArray = [];
 
   addNewServerStatus = false;
   serverCreated = false;
@@ -14,6 +17,11 @@ export class ServersComponent {
   onServerCreation() {
     this.serverCreated = true;
     this.servers.push(this.serverName);
+  }
+
+  onDisplayDetailsClicked() {
+    this.secretShown = !this.secretShown;
+    this.eventsArray.push(new Date());
   }
 
 }
