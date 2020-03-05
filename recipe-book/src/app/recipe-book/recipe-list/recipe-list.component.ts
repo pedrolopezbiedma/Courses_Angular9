7cceac181eef4c990ba4ec0780222a98cd98651d
Pedro <pedrolopezbiedma@gmail.com>
2020-03-05 16:41:05 +0100

    Saving in the middle of a video

--- recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 
 export class RecipeListComponent {
+    recipes: Recipe[] = [
+        new Recipe('A test recipe', 'This is simply a test',
+        'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg')
+    ];
     constructor() { }
 
 }
