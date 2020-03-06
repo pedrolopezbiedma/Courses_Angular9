77cbdbba25796820f355dcbe7f562bd6c48d6649
Pedro <pedrolopezbiedma@gmail.com>
2020-03-06 15:01:42 +0100

    Ending the section for thEnding the initial section for the course project

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../models/ingredient.model';
+
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css']
 })
 
-export class ShoppingListComponent  {
+export class ShoppingListComponent {
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 2)
+    ];
+
     constructor() { }
 
 }
