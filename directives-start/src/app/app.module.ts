2979e801e8a9028004801d77aa2985b356a0b91b
Pedro Lopez <xnaildev@gmail.com>
2020-03-12 12:01:30 +0100

    Finishing Section 7

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { appBasicHighlightDirective } from './Directives/BasicHighlight/basic-highlight.directive';
 import { appBetterHighlightDirective } from './Directives/BetterHighlight/better-highlight.directive';
+import { OnlyTrueDirective } from './Directives/OnlyTrue/onlytrue.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     appBasicHighlightDirective,
-    appBetterHighlightDirective
+    appBetterHighlightDirective,
+    OnlyTrueDirective
   ],
   imports: [
     BrowserModule,
