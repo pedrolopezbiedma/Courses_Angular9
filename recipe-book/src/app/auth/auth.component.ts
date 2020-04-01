ddb127d86133a604ec0bca7bea8d229b70cf97e7
Pedro Lopez <xnaildev@gmail.com>
2020-04-01 15:52:54 +0200

    Updating form & buttons pending on the mode we are

--- recipe-book/src/app/auth/auth.component.ts
+++ recipe-book/src/app/auth/auth.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  loginMode = false;
 
   constructor() { }
 
