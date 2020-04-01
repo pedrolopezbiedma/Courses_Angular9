538517aeca0025050844c284c6312a83a1478470
Pedro Lopez <xnaildev@gmail.com>
2020-04-01 16:22:15 +0200

    Including functionality in the auth form

--- recipe-book/src/app/auth/auth.component.ts
+++ recipe-book/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -13,4 +14,8 @@ export class AuthComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(authForm: NgForm){
+    console.log(authForm.value);
+    authForm.reset();
+  }
 }
