4e3dd5a70d6880661a2739846cab739ef41c5cf5
Pedro Lopez <xnaildev@gmail.com>
2020-04-02 08:25:08 +0200

    Using the authentication information

--- recipe-book/src/app/navbar/navbar.component.ts
+++ recipe-book/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../services/data-storage.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
     selector: 'app-navbar',
@@ -7,16 +9,35 @@ import { DataStorageService } from '../services/data-storage.service';
     styleUrls:['./navbar.component.css']
 })
 
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy{
+    authSubscription = new Subscription();
+    isAuthenticated = false;
     title = 'Navbar';
 
-    constructor(private dataStorage: DataStorageService) { }
+    constructor(private dataStorage: DataStorageService,
+                private authService: AuthService) { }
 
+    ngOnInit(){
+        this.authSubscription = this.authService.authenticated
+            .subscribe(authenticatedUser => {
+                console.log('Usuario authenticado en el navbar');
+                console.log(authenticatedUser);
+                authenticatedUser ? this.isAuthenticated = true : this.isAuthenticated = false;
+            })
+    }
     onStoreRecipes(){
         this.dataStorage.storeRecipes();
     }
 
     onFetchRecipes(){
         this.dataStorage.fetchRecipes();
+    }
+
+    onLogout(){
+
+    }
+    
+    ngOnDestroy(){
+        this.authSubscription.unsubscribe();
     }
 }
