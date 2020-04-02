60e30bc6b2abb3a6c905f75c293ca9549aba60ec
Pedro Lopez <xnaildev@gmail.com>
2020-04-02 07:34:06 +0200

    Improving error handling

--- recipe-book/src/app/services/auth.service.ts
+++ recipe-book/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -15,24 +15,7 @@ export class AuthService {
                password: password,
                returnSecureToken: true
            } 
-        ).pipe(catchError(responseError => {
-            let errorMessage = '';
-            if(!responseError.error.error || !responseError.error.error.message){
-                errorMessage = 'An error ocurred.';
-                return throwError(errorMessage);
-            }
-
-            switch (responseError.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'The email address is already in use by another account.';
-                    break;
-            
-                default:
-                    errorMessage = 'An error ocurred.';
-                    break;
-            }
-            return throwError(errorMessage);
-        }));
+        ).pipe(catchError(this.handleError));
     }
 
     logIn(email: string, password: string){
@@ -43,6 +26,29 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             } 
-        )
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(responseError: HttpErrorResponse){
+        let errorMessage = '';
+        if(!responseError.error.error || !responseError.error.error.message){
+            errorMessage = 'An error ocurred.';
+            return throwError(errorMessage);
+        }
+
+        switch (responseError.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account.';
+                break;
+        
+            case 'INVALID_PASSWORD':
+                errorMessage = 'The password is invalid or the user does not have a password.';
+                break;
+                
+            default:
+                errorMessage = 'An error ocurred.';
+                break;
+        }
+        return throwError(errorMessage);        
     }
 }
