87eb3a47ddd67620d366b4afa9daf811e3b06e28
Pedro Lopez <xnaildev@gmail.com>
2020-04-09 10:26:21 +0200

    Finishing the 'animations' section

--- Courses Example Apps/animations-start/src/app/app.component.ts
+++ Courses Example Apps/animations-start/src/app/app.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [ 
+      state('normal', style({ 
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [ 
+      state('normal', style({ 
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(500)),
+      transition('shrunken <=> *', animate(700))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
       this.list.push(item);
+    }
+
+    onAnimate(){
+      this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+
+    onShrunk(){
+      this.wildState = 'shrunken';
     }
 }
