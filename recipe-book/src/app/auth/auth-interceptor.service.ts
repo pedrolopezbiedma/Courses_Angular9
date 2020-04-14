54d500f83edc92c10bcb2f10337ec541e767ebdd
Pedro Lopez <xnaildev@gmail.com>
2020-04-14 17:13:13 +0200

    Installing ngrx effects package

--- recipe-book/src/app/auth/auth-interceptor.service.ts
+++ recipe-book/src/app/auth/auth-interceptor.service.ts
@@ -16,6 +16,19 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+    /**
+    if(!this.store.select('auth')){
+        return next.handle(req);
+    }
+
+    let editedRequest = req.clone({ 
+        params: new HttpParams().set('auth', this.authService.token)
+    });
+
+    return next.handle(editedRequest);
+      */
+
     return this.store.select('auth').pipe(
       take(1),
       map(authState => {
